259c4a7b615f531db23056b46beef27cbdd3c503
Nick Hoszko <nhoszko@gmail.com>
2021-10-07 22:18:57 +1100

    added a MealItemForm

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './MealItem.module.css';
 
+import MealItemForm from './MealItemForm';
+
 const MealItem = props => {
   const price = `$${props.price.toFixed(2)}`;
 
@@ -10,7 +12,7 @@ const MealItem = props => {
       <div className={styles.description}>{props.description}</div>
       <div className={styles.price}>{price}</div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   )

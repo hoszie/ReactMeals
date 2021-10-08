3e2fb48869fdd964aa28a7469ab4c94659dfb221
Nick Hoszko <nhoszko@gmail.com>
2021-10-08 23:03:01 +1100

    worked with Refs and forward Refs

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './MealItem.module.css';
 
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = props => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price
+    });
+  }
 
   return (
     <li className={styles.meal}>
@@ -12,7 +23,7 @@ const MealItem = props => {
       <div className={styles.description}>{props.description}</div>
       <div className={styles.price}>{price}</div>
       <div>
-        <MealItemForm id={props.id}/>
+        <MealItemForm onAddToCart={addToCartHandler}/>
       </div>
     </li>
   )

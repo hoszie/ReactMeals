1f60f9a35e131d2332faf0c1f1c3df803aee22a9
Nick Hoszko <nhoszko@gmail.com>
2021-10-09 01:20:32 +1100

    used useEffect for a small animation bump on the HeaderCartButton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCartButton.module.css";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context"; 
 
 const HeaderCartButton = props => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items])
+
   return (
-    <button className={styles.button} onClick={props.onClicked}>
+    <button className={btnClasses} onClick={props.onClicked}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
